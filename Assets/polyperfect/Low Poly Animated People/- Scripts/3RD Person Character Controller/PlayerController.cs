5b0f4295d46f891c30b327684b3f27f2c5032edc
Xion079 <rhdudals98@gmail.com>
2024-06-26 14:11:06 +0900

    에셋 등록 및 카메라 실험

---
+++ Assets/polyperfect/Low Poly Animated People/- Scripts/3RD Person Character Controller/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed, walkAnimationSpeed, dashPower;
+    public Animator animator;
+    public new Rigidbody rigidbody;
+
+
+    Vector3 translation;
+
+    bool canDash;
+    void Start()
+    {
+        canDash = true;
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        float speed = walkSpeed;
+        float animSpeed = walkAnimationSpeed;
+
+
+        translation = Vector3.forward * (vertical * Time.deltaTime);
+        translation += Vector3.right * (horizontalMove * Time.deltaTime);
+        translation *= speed;
+        transform.Translate(translation, Space.World);
+        if (Input.GetKey(KeyCode.LeftShift) && canDash)
+        {
+            canDash = false;
+            StartCoroutine(Dash());
+        }
+
+        animator.SetFloat("Vertical", vertical, 0.1f, Time.deltaTime);
+        animator.SetFloat("Horizontal", horizontalMove, 0.1f, Time.deltaTime);
+        animator.SetFloat("WalkSpeed", animSpeed);
+
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+
+        //근접 공격
+
+        //원거리 공격
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+    IEnumerator Dash()
+    {
+        rigidbody.AddForce(translation * dashPower, ForceMode.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        rigidbody.velocity = Vector3.zero;
+        canDash = true;
+    }
+}
